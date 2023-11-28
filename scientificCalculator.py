c06584bf477a3204ec4031eb97be3149e32af0ac
Alina Mathew <42777297+alina-m00@users.noreply.github.com>
2023-11-27 20:06:33 -0600

    Updating scientificCalculator.py
    adding the complete source code

--- scientificCalculator.py
+++ scientificCalculator.py
@@ -58,3 +58,70 @@ class Calculator:
 
             self.display.set(result_str)
             self.expression = result_str
+
+    def create_gui(self, gui):
+        gui.geometry("320x500+400+400")
+        gui.resizable(0, 0)
+        gui.title("Unique Bonus Calculator")
+
+        display_label = Label(
+            gui,
+            text="Expression",
+            anchor=SE,
+            font=("Calibri Math", 20),
+            textvariable=self.display,
+            background="#ffffff",
+            fg="#000000"
+        )
+        display_label.pack(expand=True, fill="both")
+
+        button_layout = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            ["0", ".", "=", "+"],
+            ["AC", "C"]
+        ]
+
+        for row in button_layout:
+            self.create_button_frame(gui, row)
+
+    def create_button_frame(self, gui, buttons):
+        frame = Frame(gui, bg="#000000")
+        frame.pack(expand=True, fill="both")
+
+        for text in buttons:
+            if text == "AC":
+                button = Button(
+                    frame,
+                    text=text,
+                    font=("Calibri", 22),
+                    relief=GROOVE,
+                    border=0,
+                    command=self.clear_input
+                )
+            else:
+                button = Button(
+                    frame,
+                    text=text,
+                    font=("Calibri", 22),
+                    relief=GROOVE,
+                    border=0,
+                    command=lambda t=text: self.on_button_click(t)
+                )
+            button.pack(side=LEFT, expand=True, fill="both")
+
+    def on_button_click(self, button_text):
+        if button_text.isdigit() or button_text == ".":
+            self.append_digit(button_text)
+        elif button_text in ["+", "-", "*", "/"]:
+            self.handle_operator(button_text)
+        elif button_text == "=":
+            self.calculate_result()
+        elif button_text == "C":
+            self.clear_input()
+
+if __name__ == "__main__":
+    guiWindow = tkinter.Tk()
+    calculator = Calculator(guiWindow)
+    guiWindow.mainloop()
